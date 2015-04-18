75a5a14b5a81d00cf858b2ba3400c1880e5878ef
Masamoto Miyata <miyata@sincere-co.com>
2015-04-18 09:53:18 +0900

    Initial Commit

---
+++ JsNoty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsNoty
+//
+//  Created by Masamoto Miyata on 2015/04/18.
+//  Copyright (c) 2015年 Masamoto Miyata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JsNotyTests/JsNotyTests.swift
@@ -0,0 +1,36 @@
+//
+//  JsNotyTests.swift
+//  JsNotyTests
+//
+//  Created by Masamoto Miyata on 2015/04/18.
+//  Copyright (c) 2015年 Masamoto Miyata. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JsNotyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
